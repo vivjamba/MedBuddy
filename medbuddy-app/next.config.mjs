0ff1eb45e4151f34885d6659905ba8f727e4f31d
Pulkit Saxena <pulkit@Pulkits-MacBook-Pro.local>
2025-11-08 23:07:50 +0100

    chore(update): env vars

--- medbuddy-app/next.config.mjs
+++ medbuddy-app/next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   env: {
-    FHIR_REDIRECT_URI: 'http://localhost:3000/callback', // default for local dev
+    FHIR_REDIRECT_URI: 'https://medbuddy-mu.vercel.app/callback', // default for local dev
     FHIR_CLIENT_ID: 'Il0LuXZwxRaogC9NbOn6BM9DJnYYKhJN',
-    FHIR_ISSUER: 'https://launch.smarthealthit.org/v/r4/sim/WzMsIiIsIiIsIkFVVE8iLDAsMCwwLCJ1c2VyLyouKiBwYXRpZW50LyouKiBvcGVuaWQgcHJvZmlsZSBmaGlyVXNlciBNZWRpY2F0aW9uUmVxdWVzdC5yZWFkIiwiaHR0cDovL2xvY2FsaG9zdDozMDAwL2NhbGxiYWNrIiwiSWwwTHVYWnd4UmFvZ0M5TmJPbjZCTTlESm5ZWUtoSk4iLCIiLCIiLCIiLCIiLDAsMCwiIl0/fhir',
+    FHIR_ISSUER: 'https://launch.smarthealthit.org/v/r4/sim/WzMsIiIsIiIsIkFVVE8iLDAsMCwwLCJ1c2VyLyouKiBwYXRpZW50LyouKiBvcGVuaWQgcHJvZmlsZSBmaGlyVXNlciBNZWRpY2F0aW9uUmVxdWVzdC5yZWFkIiwiaHR0cHM6Ly9tZWRidWRkeS1tdS52ZXJjZWwuYXBwL2NhbGxiYWNrIiwiSWwwTHVYWnd4UmFvZ0M5TmJPbjZCTTlESm5ZWUtoSk4iLCIiLCIiLCIiLCIiLDAsMCwiIl0/fhir',
   },
 };
 
